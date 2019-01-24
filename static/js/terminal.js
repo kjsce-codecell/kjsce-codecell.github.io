b95301c37f5420677a9bac670c5019ed36f9daf5
Freelancer-98 <neel12@somaiya.edu>
2019-01-24 09:10:21 +0530

    PWA day1 feedback form added

--- static/js/terminal.js
+++ static/js/terminal.js
@@ -527,7 +527,7 @@
 			window.open("https://www.kjscecodecell.com/newsletters/");
 		}
 		function feedback(){
-			window.open("https://docs.google.com/forms/d/e/1FAIpQLSdzGtKn60fMCSWavn8c2E4kQSkaDBVvRgrDdDMdMB0h8PvBKQ/viewform");
+			window.open("https://docs.google.com/forms/d/1aEBB9FWuGVv9UfYhWNmYoCz3ICYUpYZRCLrlYI0f-mM/viewform");
 		}
 		function latestTweets() {
 			$("#onlineTerminal").html($("#onlineTerminal").html().trim()+'<span class="response">Getting the latest tweets from @jsconfar</span>'+breakLine()+breakLine());
